7de9eec6fbfdb45f1eb3002296b4a6ab5c18a167
Sagid, Home(w) <kofon95@mail.ru>
2018-01-01 18:13:49 +0300

    XUnitTestProject1

---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact(DisplayName = "Dotnet Test")]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
