868853eafc01a0ff4223b10b3d3fd129d8a30280
Sagid, Home(w) <kofon95@mail.ru>
2018-01-01 15:42:53 +0300

    xunit

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace UnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact(DisplayName = "Successful test")]
+        public void XTestMethod1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact(DisplayName = "Failed test")]
+        public void XTestFail()
+        {
+            Assert.True(false);
+        }
+
+        [Theory(DisplayName = "DynamicTest")]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void DynamicTest(bool success)
+        {
+            Assert.True(success);
+        }
+    }
+}
